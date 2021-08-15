9f26793994c5c21260853a13587939d7291b0226
login_dhcp <nonamed424@gmail.com>
2021-08-15 20:12:06 +0900

    fix mistake, add icon

--- config.js
+++ config.js
@@ -12,11 +12,11 @@ configs.Imsize = [256, 256];
 //     });
 // });
 
-text=`【白いツバサ】櫻木真乃,wing0,wing1,wing2,wing3,wing4,감사제1,감사제2
-【ほわっとスマイル】櫻木真乃,1,2,3,4,,,
-【ナチュラルモード】櫻木真乃,1,2,3,,,,
-【柔らかな微笑み】風野灯織,1,2,3,,,,
-【金色の元気いっぱいガール】八宮めぐる,1,2,,,,,
+text=`Mano_P_R_01,wing0,wing1,wing2,wing3,wing4,감사제1,감사제2
+Mano_P_SSR_01,1,2,3,4,,,
+Mano_P_SSR_02,1,2,3,,,,
+Mano_P_SR_01,1,2,3,,,,
+Mano_S_SSR_01,1,2,,,,,
 `
 
 buildHTMLWithDataset(text);
--- dataset.js
+++ dataset.js
@@ -11,21 +11,24 @@ function csvToArray(data) {
 }
 
 function datasetToCheckboxesTable(data) {
-    var table = '<table id=sort_list_table>';
+    var table = '<table id=commu_list_table>';
 
     // add items
     for (var i = 0; i < data.length; i++) {
         var item = data[i];
         table += '<tr>';
         for (var j = 0; j < item.length; j++) {
-            table += '<td>';
+            table += '<td style="text-align: center">';
             table += `<label style="cursor:pointer" for="checkbox_${i}_${j}">`;
+            var IdolName = item[0].split('_')[0];
             if (item[j] !== "") {
-                table += `<input type="checkbox" data-checkbox-id="${i}_${j}" id="checkbox_${i}_${j}" class="select_one">`;
+                table += `<input type="checkbox" data-checkbox-id="${i}_${j}" id="checkbox_${i}_${j}">`;
                 if (j === 0) {
-                    table += `<img src="https://shinycolors.info/wiki/특수:넘겨주기/file/${item[j]}.png" width=200px>`;
+                    table += `<img src="https://shinycolors.info/wiki/특수:넘겨주기/file/Icon_${item[j]}.png" class='selectibleIcon'>`;
+                    table += `<span><br>${item[j]}</span>`;
                 } else {
-                    table += `${item[j]}`;
+                    table += `<img src="https://shinycolors.info/wiki/특수:넘겨주기/file/${IdolName}Icon.webp?width=96px" class='commuIcon'>`;
+                    table += `<span><br>${item[j]}</span>`;
                 }
             }
             table += '</label>';
